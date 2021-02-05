0ce6bb44ee30708319e4d85fa01f29c11a6f6932
dean <719738494@qq.com>
2021-02-06 02:41:29 +0800

    update:pom

--- build-docker.sh
+++ build-docker.sh
@@ -7,23 +7,6 @@ then
     exit 1
 fi
 echo "package and build docker image succeed！！！"
-##
-cd ./mygirl-c-app
-mvn dockerfile:build
 
-#image_name="himygirl/mygirl-c-app"
-#app_version="2.0.5"
-#docker tag ${image_name} ${image_name}:${app_version}
-
-c_container_name="hgCapp"
-docker rm -f ${c_container_name}
 docker-compose -f docker-compose-prod.yml up -d eureka-registry
 echo ${c_container_name}"容器正常启动..."
-
-
-cd ../mygirl-b-app
-mvn dockerfile:build
-b_container_name="hgBapp"
-docker rm -f ${b_container_name}
-docker-compose -f docker-compose-prod.yml up -d ${b_container_name}
-echo ${b_container_name}"容器正常启动..."
