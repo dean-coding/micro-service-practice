9358055433b57c1ca959ee4d54f9e967f28754d9
dean <719738494@qq.com>
2021-02-07 16:58:45 +0800

    add: prometheus

---
+++ producer-service/src/main/java/com/dean/practice/producer/config/MicrometerConfiguration.java
@@ -0,0 +1,24 @@
+package com.dean.practice.producer.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+/**
+ *
+ * @author Dean
+ * @date 2021-02-07
+ */
+@Component
+@Configuration
+public class MicrometerConfiguration {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
