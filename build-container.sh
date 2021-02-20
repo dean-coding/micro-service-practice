99a0406b89c387599726c904f064b397aed8adcb
dean <719738494@qq.com>
2021-02-20 11:28:05 +0800

    update: sh

--- build-container.sh
+++ build-container.sh
@@ -1,8 +1,4 @@
 ## bash
-OLD_BUILD_ID=$BUILD_ID
-echo $OLD_BUILD_ID
-BUILD_ID=DONTKILLME
-
 usage(){
    echo "Usage: `basename $0` [-s <service_name[string]>]"
    exit -1
@@ -19,20 +15,20 @@ do
 done
 DOCKER_COMPOSE_HOME='/usr/local'
 
-echo "build and up service container [start]"
-if [ -z "${service_name}" ];
-    then
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d
-    else
-        docker stop ${service_name}
-        docker rm -f ${service_name}
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d  --no-deps ${service_name}
-fi
-echo "build and up service container [over]"
+echo "build and up service[${service_name}] container [start]"
+# if [ -z "${service_name}" ];
+#    then
+#        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d
+#    else
+#        docker stop ${service_name}
+#        docker rm -f ${service_name}
+#        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d  --no-deps ${service_name}
+# fi
+echo "build and up service[${service_name}] container [over]"
 
 echo "prune none images [start]"
 docker image prune -f
 echo "prune none images [over]"
 
 sleep 10s
-${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
+# ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
