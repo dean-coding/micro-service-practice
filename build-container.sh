a68c2ce3d6ce9623f66d5f7928731940996b240c
dean <719738494@qq.com>
2021-02-20 12:39:55 +0800

    update： sh

--- build-container.sh
+++ build-container.sh
@@ -13,21 +13,22 @@ do
         ;;
     esac
 done
+DOCKER_COMPOSE_HOME='/usr/local'
 
 echo "build and up service[${service_name}] container [start]"
  if [ -z "${service_name}" ];
     then
-       `which docker-compose`  -f docker-compose-prod.yml up --force-recreate -d
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d
     else
-       `which docker` stop ${service_name}
-       `which docker` rm -f ${service_name}
-       `which docker-compose`  -f docker-compose-prod.yml up --force-recreate -d --no-deps ${service_name}
+        docker stop ${service_name}
+        docker rm -f ${service_name}
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d  --no-deps ${service_name}
 fi
 echo "build and up service[${service_name}] container [over]"
 
 echo "prune none images [start]"
-`which docker` image prune -f
+docker image prune -f
 echo "prune none images [over]"
 
 sleep 10s
-`which docker-compose` -f docker-compose-prod.yml ps
+${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
