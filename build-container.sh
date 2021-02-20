f1639d6c5626a98328bfaf0bc3692572efefd753
dean <719738494@qq.com>
2021-02-20 12:06:12 +0800

    update: sh

--- build-container.sh
+++ build-container.sh
@@ -16,14 +16,14 @@ done
 DOCKER_COMPOSE_HOME='/usr/local'
 
 echo "build and up service[${service_name}] container [start]"
-# if [ -z "${service_name}" ];
-#    then
-#        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d
-#    else
-#        docker stop ${service_name}
-#        docker rm -f ${service_name}
-#        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d  --no-deps ${service_name}
-# fi
+ if [ -z "${service_name}" ];
+    then
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d
+    else
+        docker stop ${service_name}
+        docker rm -f ${service_name}
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up --force-recreate -d  --no-deps ${service_name}
+fi
 echo "build and up service[${service_name}] container [over]"
 
 echo "prune none images [start]"
@@ -31,4 +31,4 @@ docker image prune -f
 echo "prune none images [over]"
 
 sleep 10s
-# ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
+${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
