bc6646079d19f335651404df4bfbac95d45c6019
dean <719738494@qq.com>
2021-02-21 09:35:16 +0800

    update: sh

--- build-container.sh
+++ build-container.sh
@@ -17,30 +17,29 @@ DOCKER_COMPOSE_HOME='/usr/local'
 
  if [ -z "${service_name}" ];
     then
-        echo "stop containers [start]"
+        echo "===> stop containers [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml down
-        echo "stop containers [over]"
+        echo "===> stop containers [over]"
 
-        echo "build and up service containers [start]"
+        echo "===> build and up service containers [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up
-        echo "build and up service containers [over]"
+        echo "===> build and up service containers [over]"
     else
-        echo "stop container ${service_name} [start]"
+        echo "===> stop container ${service_name} [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml stop -t 10 ${service_name}
-        echo "stop container ${service_name} [over]"
+        echo "===> stop container ${service_name} [over]"
 
-        echo "remove container ${service_name} [start]"
+        echo "===> remove container ${service_name} [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml rm -f ${service_name}
-        echo "remove container ${service_name} [over]"
+        echo "===> remove container ${service_name} [over]"
 
-        echo "build and up service[${service_name}] container [start]"
+        echo "===> build and up service[${service_name}] container [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up -d ${service_name}
-        echo "build and up service[${service_name}] container [over]"
+        echo "===> build and up service[${service_name}] container [over]"
 fi
 
-echo "prune none images [start]"
+echo "===> prune none images [start]"
 docker image prune -f
-echo "prune none images [over]"
-
-sleep 10s
+echo "===> prune none images [over]"
+sleep 6s
 ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
