e35d8363beb98c4368905bb5a19f9a702c6527b1
dean <719738494@qq.com>
2021-02-21 09:51:11 +0800

    update: sh

--- build-container.sh
+++ build-container.sh
@@ -14,27 +14,28 @@ do
     esac
 done
 DOCKER_COMPOSE_HOME='/usr/local'
+CURRENT_COMPOSE_DIR=`pwd`/docker-compose-prod.yml
 
  if [ -z "${service_name}" ];
     then
         echo "===> stop containers [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml down
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} down
         echo "===> stop containers [over]"
 
         echo "===> build and up service containers [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} up
         echo "===> build and up service containers [over]"
     else
         echo "===> stop container ${service_name} [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml stop -t 10 ${service_name}
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} stop -t 10 ${service_name}
         echo "===> stop container ${service_name} [over]"
 
         echo "===> remove container ${service_name} [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml rm -f ${service_name}
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} rm -f ${service_name}
         echo "===> remove container ${service_name} [over]"
 
         echo "===> build and up service[${service_name}] container [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml up -d ${service_name}
+        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} up -d ${service_name}
         echo "===> build and up service[${service_name}] container [over]"
 fi
 
@@ -42,4 +43,4 @@ echo "===> prune none images [start]"
 docker image prune -f
 echo "===> prune none images [over]"
 sleep 6s
-${DOCKER_COMPOSE_HOME}/bin/docker-compose -f docker-compose-prod.yml ps
+${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} ps
