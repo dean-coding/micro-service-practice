e2a2f3f090fe2099d916921b17b3f72ff4118dde
dean <719738494@qq.com>
2021-02-21 16:18:57 +0800

    update: sh & Jenkinsfile

--- build-container.sh
+++ build-container.sh
@@ -1,4 +1,6 @@
 ## bash
+## 构建并启动的新的容器
+
 usage(){
    echo "Usage: `basename $0` [-s <service_name[string]>]"
    exit -1
@@ -18,29 +20,14 @@ CURRENT_COMPOSE_DIR=`pwd`/docker-compose-prod.yml
 
  if [ -z "${service_name}" ];
     then
-        echo "===> stop containers [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} down
-        echo "===> stop containers [over]"
-
         echo "===> build and up service containers [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} up
         echo "===> build and up service containers [over]"
     else
-        echo "===> stop container ${service_name} [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} stop -t 10 ${service_name}
-        echo "===> stop container ${service_name} [over]"
-
-        echo "===> remove container ${service_name} [start]"
-        ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} rm -f ${service_name}
-        echo "===> remove container ${service_name} [over]"
-
         echo "===> build and up service[${service_name}] container [start]"
         ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} up -d ${service_name}
         echo "===> build and up service[${service_name}] container [over]"
 fi
 
-echo "===> prune none images [start]"
-docker image prune -f
-echo "===> prune none images [over]"
 sleep 6s
 ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} ps
--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,5 @@
 ## bash
+## 停止并移除旧的容器 & 构建新的镜像
 
 usage(){
    echo "Usage: `basename $0` [-s <service_name[string]>]"
@@ -15,17 +16,32 @@ do
     esac
 done
 
+DOCKER_COMPOSE_HOME='/usr/local'
+CURRENT_COMPOSE_DIR=`pwd`/docker-compose-prod.yml
 
 if [ -z "${service_name}" ];
 then
-    mvn dockerfile:build
+    echo "===> stop containers [start]"
+    ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} down
+    echo "===> stop containers [over]"
+
+    echo "===> build new images [start]"
+    `which mvn` dockerfile:build
+    echo "===> build new images [start]"
 else
-    mvn -pl ${service_name} -am dockerfile:build
+    echo "===> stop container ${service_name} [start]"
+    ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} stop -t 10 ${service_name}
+    echo "===> stop container ${service_name} [over]"
+
+    echo "===> remove container ${service_name} [start]"
+    ${DOCKER_COMPOSE_HOME}/bin/docker-compose -f ${CURRENT_COMPOSE_DIR} rm -f ${service_name}
+    echo "===> remove container ${service_name} [over]"
+
+    echo "===> build new image ${service_name} [start]"
+    `which mvn` -pl ${service_name} -am dockerfile:build
+    echo "===> build new image ${service_name} [over]"
 fi
 
-if [[ $? != "0" ]]
-then
-    echo "build docker image error"
-    exit 1
-fi
-echo "build docker image succeed！！！"
+echo "===> prune none images [start]"
+docker image prune -f
+echo "===> prune none images [over]"
